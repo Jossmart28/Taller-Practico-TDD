b8c80f5414863f0b66f0160e870f9c841be26106
Jossmart28 <jairomartinez2804@gmail.com>
2026-04-16 16:55:55 -0600

    Refactorización Final

--- rpg.py
+++ rpg.py
@@ -5,29 +5,39 @@ class Personaje:
         self.esta_vivo = True
 
     def atacar(self, objetivo, dano):
-        if not self.esta_vivo:
+        if not self._puede_atacar(objetivo):
             return
 
-        if self == objetivo:
-            return
-
-        # Ajuste por nivel
-        if self.nivel - objetivo.nivel >= 5:
-            dano *= 1.5
-        elif objetivo.nivel - self.nivel >= 5:
-            dano *= 0.5
-
-        objetivo.hp -= int(dano)
-
-        if objetivo.hp <= 0:
-            objetivo.hp = 0
-            objetivo.esta_vivo = False
+        dano = self._calcular_dano(objetivo, dano)
+        self._aplicar_dano(objetivo, dano)
 
     def curar(self, cantidad):
         if not self.esta_vivo:
             return
 
         self.hp += cantidad
-
         if self.hp > 1000:
             self.hp = 1000
+
+    # Métodos auxiliares
+
+    def _puede_atacar(self, objetivo):
+        if not self.esta_vivo:
+            return False
+        if self == objetivo:
+            return False
+        return True
+
+    def _calcular_dano(self, objetivo, dano):
+        if self.nivel - objetivo.nivel >= 5:
+            return int(dano * 1.5)
+        elif objetivo.nivel - self.nivel >= 5:
+            return int(dano * 0.5)
+        return dano
+
+    def _aplicar_dano(self, objetivo, dano):
+        objetivo.hp -= dano
+
+        if objetivo.hp <= 0:
+            objetivo.hp = 0
+            objetivo.esta_vivo = False
