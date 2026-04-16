b67501496297b5fd53d71165f74f8bc480ad1fba
Jossmart28 <jairomartinez2804@gmail.com>
2026-04-16 16:04:33 -0600

    Taller TDD - Segundo Test RED (Prueba de daño)

--- test_rpg.py
+++ test_rpg.py
@@ -8,3 +8,14 @@ def test_personaje_nace_con_estadisticas_correctas():
     assert heroe.hp == 1000
     assert heroe.nivel == 1
     assert heroe.esta_vivo == True
+
+def test_personaje_puede_danar_a_otro():
+    # Arrange
+    atacante = Personaje()
+    objetivo = Personaje()
+
+    # Act
+    atacante.atacar(objetivo, 100)
+
+    # Assert
+    assert objetivo.hp == 900
